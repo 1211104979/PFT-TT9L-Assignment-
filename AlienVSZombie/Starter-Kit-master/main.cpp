48d674bcbcdc89ec80f1054447275d135290aecc
Afiq Ikhwan <96471043+1211104979@users.noreply.github.com>
2023-01-17 19:24:00 +0800

    Add files via upload

---
+++ AlienVSZombie/Starter-Kit-master/main.cpp
@@ -0,0 +1,22 @@
+// ********************************************************* 
+// Course: TCP1101 PROGRAMMING FUNDAMENTALS 
+// Year: Trimester 1, 2022/23 (T2215) 
+// Lab: TxxL 
+// Names: MEMBER_NAME_1 | MEMBER_NAME_2 
+// IDs: MEMBER_ID_1 | MEMBER_ID_2 
+// Emails: MEMBER_EMAIL_1 | MEMBER_EMAIL_2 
+// Phones: MEMBER_PHONE_1 | MEMBER_PHONE_2  
+// ********************************************************* 
+
+// TODO: Fill in the missing information above and delete this line.
+
+#include "pf/helper.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Assignment (Part 1)" << endl;
+    cout << "Let's Get Started!" << endl;
+    pf::Pause();
+}
