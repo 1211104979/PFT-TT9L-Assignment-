b03b63df203243bc8401b8a18a28d8e3f6d9dd03
1211104979 <1211104979@student.mmu.edu.my>
2023-01-24 19:30:14 +0800

    header edit

--- AlienVSZombie/Main game/Sub/MainHeader.h
+++ AlienVSZombie/Main game/Sub/MainHeader.h
@@ -1,14 +1,37 @@
 #pragma once
-// #ifndef MAINHEADER_H
-// #define MAINHEADER_H
-    // Global 
-    //namespace gf
-        int ClearScreen();
-        int Pause();
-         
-        void getMenu();
-        void instructions();
-        void gameover();
-    
+#include <vector> 
+#ifndef MAINHEADER_H
+#define MAINHEADER_H
 
-// #endif
+    // Global 
+    int ClearScreen();
+    int Pause();
+         
+    void getMenu();
+    void instructions();
+    void gameover();
+
+
+class GameBoard 
+{ 
+private: 
+    vector< vector<char> > map_; // convention to put trailing underscore 
+    int dimX_, dimY_;            // to indicate private data 
+    
+public: 
+    GameBoard(int dimX = 3, int dimY = 5); 
+
+    void init(int dimX, int dimY); 
+    void display() ; 
+    void settings();
+    int setDimX(int x);
+    int setDimY(int y);
+
+    int getDimX() const;
+    int getDimY() const;
+
+    char getObject(int dimX, int dimY) const;
+}; 
+
+
+#endif
