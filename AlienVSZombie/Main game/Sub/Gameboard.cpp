9f8b22d55fd752e75ec25cf76838fa5e3033fdd9
1211104979 <1211104979@student.mmu.edu.my>
2023-01-26 18:56:06 +0800

    file naming

--- AlienVSZombie/Main game/Sub/Gameboard.cpp
+++ AlienVSZombie/Main game/Sub/Gameboard.cpp
@@ -1,12 +1,12 @@
-// **************************************************************************
-// Course: TCP1101 PROGRAMMING FUNDAMENTALS 
-// Year  : Trimester 1, 2022/23 (T2215) 
-// Lab   : TT9L 
-// Names : CHUA LI EN                    | MUHAMMAD AFIQ IKHWAN BIN AMINUDDIN  
-// IDs   : 1211104335                    | 1211104979 
-// Emails: 1211104335@student.mmu.edu.my | 1211104979@student.mmu.edu.my 
-// Phones: 011-1026 0985                 | 011-3174 2235 
-// **************************************************************************
+// ************************************************************************** //
+// Course: TCP1101 PROGRAMMING FUNDAMENTALS                                   //
+// Year  : Trimester 1, 2022/23 (T2215)                                       //
+// Lab   : TT9L                                                               //
+// Names : CHUA LI EN                    | MUHAMMAD AFIQ IKHWAN BIN AMINUDDIN // 
+// IDs   : 1211104335                    | 1211104979                         //
+// Emails: 1211104335@student.mmu.edu.my | 1211104979@student.mmu.edu.my      //
+// Phones: 011-1026 0985                 | 011-3174 2235                      //
+// ************************************************************************** //
 
 #include "MainHeader.h"
 #include <iostream>
@@ -15,7 +15,6 @@
 #include <cstdlib> // for system()
 #include <ctime>   // for time() in srand( time(NULL) );
 #include <iomanip> // for setw()
-#include <conio.h>
 
 GameBoard::GameBoard(int dimX, int dimY, int zomb) // liEn & Afiq (from tutorial)
 {
---
+++ AlienVSZombie/Main game/Sub/movement.cpp
@@ -0,0 +1,13 @@
+// ************************************************************************** //
+// Course: TCP1101 PROGRAMMING FUNDAMENTALS                                   //
+// Year  : Trimester 1, 2022/23 (T2215)                                       //
+// Lab   : TT9L                                                               //
+// Names : CHUA LI EN                    | MUHAMMAD AFIQ IKHWAN BIN AMINUDDIN //
+// IDs   : 1211104335                    | 1211104979                         //
+// Emails: 1211104335@student.mmu.edu.my | 1211104979@student.mmu.edu.my      //
+// Phones: 011-1026 0985                 | 011-3174 2235                      //
+// ************************************************************************** //
+
+#include "MainHeader.h"
+#include <iostream>
+
