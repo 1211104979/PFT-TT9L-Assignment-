f5002e7c8d1bb342405609194113c74befa1e657
1211104979 <1211104979@student.mmu.edu.my>
2023-02-01 20:06:43 +0800

    edit

--- AlienVSZombie/Main game/main.cpp
+++ AlienVSZombie/Main game/main.cpp
@@ -10,32 +10,13 @@
 
 
 // Youtube video : https://youtu.be/_INcX8hO_GQ
-
-// TODO: Fill in the missing information above and delete this line.
+//===========================================================================================================================
+// TODO:
 
 //Game Commands 
     //Movement (up, down, left, right)
     //Change direction of arrow (arrow)
-    //Display user commands (help)===
-    //Save game (save)===
     //Load game (load)
-    //Quit game (quit)===
-
-//Board setting (must be odd numbers)
-    //Board size and config ===
-    //Random generated Objects ("h"=health pack, "r"=rock/block, "<"/"^"/">"/"v"= direction pad, " "= empty space)===
-
-//Graphic on board
-    //Zombie setting (number of zombie, denoted as int/number)===
-    //Alien (denoted as A)===
-
-//Stats
-    //Alien stat (Life, Attack)===
-    //Zombie stat (Life, Attack, Range)===
-
-//Spawn
-    //Alien (position will initially be at the middle)===
-    //Zombie (random)===
 
 //Turns
     //Display (who turn it is)
@@ -43,6 +24,7 @@
 //AI movement program
     //Move towards Alien
     //Attack when in range
+//===========================================================================================================================
 
 #include "Sub/MainHeader.h"
 #include <iostream>
